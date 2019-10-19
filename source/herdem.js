d9f94a5726fea6cc4423ab818d9583221334268e
kelvin <kelvinperrie@gmail.com>
2019-10-19 14:11:18 +1300

    keeping within the boundaries of the canvas and respecting the moving objects size

--- source/herdem.js
+++ source/herdem.js
@@ -2,34 +2,49 @@
 var Mover = function(settings) {
     var self = this;
 
-    self.current = { x: 100, y: 100 };
-    self.target = { x: 100, y: 100 };
+    self.world = settings.world;
+    console.log("hi - " + self.world.height);
+    self.width = 40;
+    self.height = 40;
+    // put this somewhere within the bound of the page
+    var x = getRandomInt(0 + self.width / 2,self.world.width - self.width / 2)
+    var y = getRandomInt(0 + self.height / 2,self.world.height - self.height / 2)
+    self.current = { x: x, y: y };
+    self.target = { x: x, y: y };
     self.threatDistance = 100;
+    self.lastSawThreatAt = null;
+
 
     self.draw = function(ctx) {
+
+
+        ctx.save();
+        ctx.translate(self.current.x , self.current.y );
+        ctx.fillStyle = '#0000ff';
+        ctx.fillRect(- (self.width / 2), - (self.height / 2), self.width, self.height);
+        ctx.restore();
+
         // temporarily draw the target
         ctx.save();
         ctx.translate(self.target.x, self.target.y);
         ctx.fillStyle = '#ff0000';
-        ctx.fillRect(0, 0, 5, 5);
-        ctx.restore();
-
-        ctx.save();
-        ctx.translate(self.current.x, self.current.y);
-        ctx.fillStyle = '#0000ff';
-        ctx.fillRect(0, 0, 5, 5);
+        ctx.fillRect(-2, -2, 4, 4);
         ctx.restore();
     }
     self.notifyOfMouseMove = function(pointerLocation) {
-        var a = self.current.x - pointerLocation.x;
-        var b = self.current.y - pointerLocation.y;
+        self.lastSawThreatAt = pointerLocation;
+    }
+    self.checkIfNeedToFlee = function() {
+        if(!self.lastSawThreatAt) return;
+        var a = self.current.x - self.lastSawThreatAt.x;
+        var b = self.current.y - self.lastSawThreatAt.y;
 
         var distance = Math.sqrt( a*a + b*b );
         if(distance < self.threatDistance) {
-            self.triggerFlee(pointerLocation);
+            self.setFleeTarget(self.lastSawThreatAt);
         }
     }
-    self.triggerFlee = function(threatLocation) {
+    self.setFleeTarget = function(threatLocation) {
         console.log("triggerFlee");
         // pick a target in the opposite direction of the threat
         // work out the angle between where we are and the current threat position
@@ -37,18 +52,32 @@ var Mover = function(settings) {
         var delta_y = threatLocation.y - self.current.y;
         var theta_radians = Math.atan2(delta_y, delta_x)
 
-        var fleeDistance = 30;
+        var fleeDistance = 50;
 
         // what is the adjacent (X)
         var adjacent = Math.cos(theta_radians) * fleeDistance;
         // what is the opposite (Y)
         var opposite = Math.sin(theta_radians) * fleeDistance;
 
-        self.target = { x: self.current.x - adjacent, y: self.current.y - opposite};
+        // don't let the target be closer to the edge than this object will fit
+        var targetX = self.current.x - adjacent;
+        if(targetX < self.width / 2) {
+            targetX = self.width / 2;
+        } else if(targetX > self.world.width - (self.width / 2)) {
+            targetX = self.world.width - (self.width / 2);
+        }
+        var targetY = self.current.y - opposite;
+        if(targetY < self.height / 2) {
+            targetY = self.height / 2;
+        } else if(targetY > (self.world.height - (self.height / 2))) {
+            targetY = self.world.height - (self.height / 2);
+        }
+        self.target = { x: targetX, y: targetY };
         console.log("my new target is ");
         console.log(self.target);
     }
     self.move = function() {
+        self.checkIfNeedToFlee();
         if(self.target.x === self.current.x && self.target.y === self.current.y) {
             return;
         }
@@ -57,7 +86,7 @@ var Mover = function(settings) {
         var delta_y = self.target.y - self.current.y;
         var theta_radians = Math.atan2(delta_y, delta_x)
 
-        var travelDistance = 2;
+        var travelDistance = 5;
 
         var distanceToTarget = Math.sqrt( delta_x*delta_x + delta_y*delta_y );
         // if we're very close then set us at the target
@@ -85,9 +114,16 @@ var World = function(settings){
     var self = this;
 
     self.movers = [];
-    var newMover = new Mover();
-    self.movers.push(newMover);
+    self.width = 800;
+    self.height = 600;
+    var moverSettings = {
+        world : this
+    }
 
+    for (var i = 0; i < 100; i++) {
+        var newMover = new Mover(moverSettings);
+        self.movers.push(newMover);
+    }
     self.draw = function(context) {
         for(var i = 0; i < self.movers.length; i++) {
             self.movers[i].draw(context);
@@ -111,4 +147,9 @@ var World = function(settings){
         self.notifyOfMouseMove(event);
     });
 };
-var world = new World();
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
